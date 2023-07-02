7ff5abec259cae62573ecb8a803ade75a6970d92
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-02 07:09:55 +0600

    bottomSheet is added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,11 +12,37 @@ class HomeScreen extends StatefulWidget{
 }
 
 class HomeScreenUI extends State<HomeScreen>{
+
+  void showBottomSheetForTakingInputNote(){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text("Add a Note"),
+                TextFormField(),
+                TextFormField(),
+                ElevatedButton(onPressed: (){}, child: Text("Add Note"))
+              ],
+            ),
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("ToDo App 009"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          showBottomSheetForTakingInputNote();
+        },
       ),
     );
   }
